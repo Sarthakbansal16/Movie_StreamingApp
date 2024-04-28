ee5d2744d151ba0b3e2807bc50ab925ae6c6913b
Sarthak Bansal <sarthakbansal087@gmail.com>
2024-04-28 10:20:17 +0530

    written getuser,getallusers and getstats controller

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -44,7 +44,9 @@ const deleteUser = async(req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       try {
         await User.findByIdAndDelete(req.params.id);
-        throw new ApiResponse(200,"User has been deleted")
+        return res
+        .status(200)
+        .json(new ApiResponse(200,"User has been deleted"))
       } catch (err) {
         throw new ApiError(500,err?.message)
       }
@@ -53,4 +55,60 @@ const deleteUser = async(req, res) => {
     }
   }
 
-export{updateUser,deleteUser}
+//get current user
+  const getUser = async(req, res) => {
+    
+    try {
+        const user=await User.findById(req.params.id).select("-password");
+        return res
+        .status(200)
+        .json( new ApiResponse(200,user,"fetched successfully"))
+      } catch (err) {
+        throw new ApiError(500,err?.message)
+      }
+  }
+
+// get all users
+const getAllUsers = async(req,res) => {
+  const query=req.query.new;
+  if(req.user.isAdmin){
+    try{
+      const users= query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+      return res
+        .status(200)
+        .json( new ApiResponse(200,users,"All users fetched"))
+    }catch(err){
+      throw new ApiError(500,err.message)
+    }
+  }else{
+    throw new ApiError(403,"You are not allowed to see all users!");
+  }
+}
+
+const userStats = async (req, res) => {
+  const today = new Date();
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    return res
+    .status(200)
+    .json(new ApiResponse(200,data,"All data is fetched and shown"))
+  } catch (err) {
+    throw new ApiError(500,err?.message)
+  }
+}
+
+export{updateUser,deleteUser,getAllUsers,getUser,userStats}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { updateUser,deleteUser } from "../controllers/User.controller.js";
+import { updateUser,deleteUser, getUser, getAllUsers, userStats } from "../controllers/User.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router= Router()
 
 router.put("/update/:id", verifyJWT, updateUser);
 router.delete("/delete/:id", verifyJWT, deleteUser);
-
+router.get("/finduser/:id",getUser)
+router.get("/",verifyJWT,getAllUsers)
+router.get("/stats",userStats)
 // router.route("/update").put("/:id",verifyJWT,updateUser)
 // router.route("/delete").delete("/:id",verifyJWT,deleteUser)
 
