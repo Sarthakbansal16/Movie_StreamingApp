8561dd1cb8302f348bffcb12fbe049dc69fd85fb
Sarthak Bansal <sarthakbansal087@gmail.com>
2024-04-29 13:08:17 +0530

    completed all controllers and started frontend

--- backend/controllers/List.controller.js
+++ backend/controllers/List.controller.js
@@ -1,15 +1,16 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import {List} from "../models/list.models.js"
+// import { addListener } from "nodemon";
 
-
+//create the list
 const createList = async (req, res) => {
     if (req.user.isAdmin) {
       try {
         const newList = await List.create(req.body);
         return res
           .status(201)
-          .json(new ApiResponse(200, newList, "Movie is added successfully"));
+          .json(new ApiResponse(200, newList, "List is added successfully"));
       } catch (err) {
         throw new ApiError(500, err?.message);
       }
@@ -18,4 +19,51 @@ const createList = async (req, res) => {
     }
   };
 
-  export {createList}
+    //delete the list
+    const deleteList = async (req, res) => {
+        if (req.user.isAdmin) {
+          try {
+            await List.findByIdAndDelete(req.params.id)
+            return res
+              .status(200)
+              .json(new ApiResponse(200,{}, "List has been deleted"));
+          } catch (error) {
+            throw new ApiError(500, error.message);
+          }
+        }
+      };  
+    
+    //get list
+
+    const getList = async(req,res) => {
+        const typeQuery= req.query.type;
+        const genreQuery= req.query.genre;
+        let list=[];
+        try{
+            if(typeQuery){
+                if(genreQuery){
+                    list=await List.aggregate([
+                        {$sample: {size:10}},
+                        {$match : {type:typeQuery, genre: genreQuery}}
+                    ]);
+                }
+                else{
+                    list = await List.aggregate([
+                        {$sample: {size:10}},
+                        {$match: {type:typeQuery}},
+                    ]);
+                }
+            }
+            else{
+                list = await List.aggregate([{$sample: {size:10}}]);
+            }
+            return res
+              .status(200)
+              .json(new ApiResponse(200,list, "List is fetched successfully"));
+        }catch(err){
+            throw new ApiError(500,err?.message)
+        }
+     }
+     
+
+  export {createList,deleteList,getList}
--- backend/routes/list.route.js
+++ backend/routes/list.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
-import { createList } from "../controllers/List.controller.js";
+import { createList, deleteList, getList } from "../controllers/List.controller.js";
 
 const router=Router()
 
 router.post("/",verifyJWT,createList)
+router.delete("/deletelist/:id",verifyJWT,deleteList)
+router.get("/",verifyJWT,getList)
 
 export default router
